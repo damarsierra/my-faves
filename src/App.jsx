4cdccca480bb02edd9df65f4ad3c8dfc254b282d
Damar Sierra <damarsierra@gmail.com>
2023-06-08 22:46:44 -0400

    set up app, connected spotify web api

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,58 @@
 import * as React from 'react';
+import axios from 'axios';
+import qs from 'qs';
+
+const client_id = import.meta.env.VITE_APP_SPOTIFY_CLIENT_ID
+const client_secret = import.meta.env.VITE_APP_SPOTIFY_CLIENT_SECRET
+const refresh_token = import.meta.env.VITE_APP_SPOTIFY_REFRESH_TOKEN
+
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+const ARTIST_ENDPOINT = `https://api.spotify.com/v1/artists`;
+
+const getAccessToken = async () => {
+  const basic = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
+  const response = await fetch(TOKEN_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${basic}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: qs.stringify({
+      grant_type: "refresh_token",
+      refresh_token,
+    }),
+  });
+  return response.json();
+}
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [artist, setArtist] = React.useState({});
+
+  const artistId = `7nqOGRxlXj7N2JYbgNEjYH`;
+
+  const getArtist = async (client_id, client_secret, refresh_token, artistId) => {
+    const { access_token } = await getAccessToken(client_id, client_secret, refresh_token);
+    const { data } = await axios.get(`${ARTIST_ENDPOINT}/${artistId}`, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      }
+    });
+
+    setArtist(data);
+  }
+
+  React.useEffect(() => {
+    getArtist(client_id, client_secret, refresh_token, artistId);
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Hello!</h1>
+      
+      <p>{artist.name}</p>
+    </div>
   )
 }
 
