94f053a1a38edac462ed5fdf22a3615706866dee
Damar Sierra <damarsierra@gmail.com>
2023-06-11 18:50:16 -0400

    added bootstrap and react-bootstrap, fine tuned how to fetch from spotifyAPI, albums state added to Row component for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import axios from 'axios';
 import qs from 'qs';
+import * as RB from 'react-bootstrap';
 
 const client_id = import.meta.env.VITE_APP_SPOTIFY_CLIENT_ID
 const client_secret = import.meta.env.VITE_APP_SPOTIFY_CLIENT_SECRET
 const refresh_token = import.meta.env.VITE_APP_SPOTIFY_REFRESH_TOKEN
 
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
-const ARTIST_ENDPOINT = `https://api.spotify.com/v1/artists`;
+const ARTIST_ENDPOINT = `https://api.spotify.com/v1/me/top/artists`;
 
 const getAccessToken = async () => {
   const basic = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
@@ -25,35 +26,82 @@ const getAccessToken = async () => {
   return response.json();
 }
 
+const App = () => {
+  const [artists, setArtists] = React.useState([]);
 
-
-function App() {
-  const [artist, setArtist] = React.useState({});
-
-  const artistId = `7nqOGRxlXj7N2JYbgNEjYH`;
-
-  const getArtist = async (client_id, client_secret, refresh_token, artistId) => {
-    const { access_token } = await getAccessToken(client_id, client_secret, refresh_token);
-    const { data } = await axios.get(`${ARTIST_ENDPOINT}/${artistId}`, {
+  const fetchItems = async (endpoint, setState) => {
+    const { access_token } = await getAccessToken();
+    const { data } = await axios.get(endpoint, {
       headers: {
         Authorization: `Bearer ${access_token}`,
       }
     });
-
-    setArtist(data);
+    setState(data.items);
+    // setAlbums(data.items.map(item => {
+    //   return getAlbums(item.href)
+    // }));
   }
 
   React.useEffect(() => {
-    getArtist(client_id, client_secret, refresh_token, artistId);
-  });
+      fetchItems(ARTIST_ENDPOINT, setArtists);
+  }, []);
 
   return (
-    <div>
-      <h1>Hello!</h1>
+    <div className='col-lg-8 mx-auto p-4 py-md-5'>
+      <header className='d-flex align-items-center pb-3 mb-5 border-bottom'>
+        <h1 className='h1'>Hello!</h1>
+        
+      </header>
+      <main>
+        <p className="fs-5 col-md-8">These are my most listened to artists on spotify!</p>
+        <RB.Table hover>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Image</th>
+              <th>Albums</th>
+            </tr>
+          </thead>
+          <tbody>
+            {artists.map(artist => <Row key={artist.id} artist={artist} />)}
+          </tbody>
+        </RB.Table>
+      </main>
       
-      <p>{artist.name}</p>
     </div>
   )
+}
+
+const Row = ({ artist }) => {
+  const [albums, setAlbums] = React.useState([]);
+
+  const getAlbums = async (href) => {
+    const { access_token } = await getAccessToken();
+    const endpoint = `${href}/albums?limit=3`;
+    const { data } = await axios.get(endpoint, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      }
+    });
+    // console.log(data.items);
+    setAlbums(data.items);
+  }
+
+  React.useEffect(() => {
+    getAlbums(artist.href);
+    console.log(albums);
+  }, []);
+
+  return (
+    <tr>
+      <th scope="row">{artist.name}</th>
+      <td><RB.Image src={artist.images[0].url} alt={artist.name} height="150" rounded /></td>
+      <td>
+        <RB.ListGroup>
+          {albums.map((album) => <RB.ListGroup.Item key={album.id}>{album.name}</RB.ListGroup.Item>)}
+        </RB.ListGroup></td>
+    </tr>
+  );
 }
 
 export default App
